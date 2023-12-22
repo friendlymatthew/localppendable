e4b312659a640a41f5ea9b447acbee2f2209690f
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2023-12-22 17:56:33 -0500

    ace editor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use warp::Filter;
 use warp::fs::dir;
+use tracing::{Level};
+use tracing_subscriber::fmt;
 
 #[tokio::main]
 async fn main() {
+
+    fmt::Subscriber::builder()
+        .with_max_level(Level::INFO)
+        .with_writer(std::io::stdout)
+        .fmt_fields(fmt::format::PrettyFields::new())
+        .init();
+
     let dir = dir("./files");
 
     warp::serve(dir)
